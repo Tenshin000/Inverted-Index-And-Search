9ea2efc6a0d41f61a2004d28c0d6c1c7f90b37dd
Hajar537 <111440574+Hajar537@users.noreply.github.com>
2025-05-22 15:55:11 +0200

    Update search-query.py

--- non-parallel solution/search-query.py
+++ non-parallel solution/search-query.py
@@ -1,31 +1,32 @@
-import sys
+import sys 
 import os
 import glob
 
+# The function takes the query, splits it into words, and convert them to lower case for case insensitivity
 def search_inverted_index(query, index_dir):
     query_terms = set(term.lower() for term in query.split())
     if not query_terms:
         return ["Error: Empty query."]
 
-    term_to_files = {term: set() for term in query_terms}
-    for filepath in glob.glob(os.path.join(index_dir, "part-*")):
-        with open(filepath, 'r', encoding='utf-8') as f:
+    term_to_files = {term: set() for term in query_terms} # it collect the files name of the terms
+    for filepath in glob.glob(os.path.join(index_dir, "part-*")): # This assumes the inverted index is split across multiple files (common in Hadoop or Spark).
+        with open(filepath, 'r', encoding='utf-8') as f: 
             for line in f:
-                line = line.strip()
+                line = line.strip()  #removes any whitespaces or newlines from the line
                 if line.startswith("("):  # Spark format
-                    word, file_list = eval(line)
+                    word, file_list = eval(line)  #parse the word and its filename into one unit
                 else:  # Hadoop format
                     word, files = line.split("\t")
                     file_list = files.split(",")
 
                 word = word.lower()
-                if word in query_terms:
+                if word in query_terms:   # taking only the filename without its counting file
                     for file_count in file_list:
                         filename = file_count.split(":")[0]
                         term_to_files[word].add(filename)
 
-    result_files = term_to_files[list(query_terms)[0]]
-    for term in query_terms:
+    result_files = term_to_files[list(query_terms)[0]] #This serves as the starting point for finding files that contain all query terms
+    for term in query_terms: # filtering
         result_files = result_files.intersection(term_to_files[term])
 
     return sorted(list(result_files)) if result_files else ["No matches found."]
